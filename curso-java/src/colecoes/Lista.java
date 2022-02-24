0d57f0194dc3be9b7a1c9b6fb4bb17c01a25b6c9
Felipe <ficechin@hotmail.com>
2022-02-24 16:59:45 -0300

    Streams project.

--- curso-java/src/colecoes/Lista.java
+++ curso-java/src/colecoes/Lista.java
@@ -1,34 +1,56 @@
 package colecoes;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Lista {
 
-	public static void main(String[] args) {
-		List<Usuario> lista = new ArrayList<>();
-		
-		Usuario u1 = new Usuario("Ana");
-		lista.add(u1);
-		
-		lista.add(new Usuario("Carlos"));
-		lista.add(new Usuario("Lia"));
-		lista.add(new Usuario("Bia"));
-		lista.add(new Usuario("Manu"));
-		lista.add(new Usuario("Manu"));
-		
-		System.out.println(lista.get(3)); // acessar pelo índice
-		
-		System.out.println(">>>> " + lista.remove(1));
-		System.out.println(lista.remove(new Usuario("Manu")));
-		System.out.println(lista.remove(new Usuario("Manu")));
-		System.out.println(lista.remove(new Usuario("Manu")));
-		
-		System.out.println("Tem? " + lista.contains(new Usuario("Lia")));
-		System.out.println("Tem? " + lista.contains(u1));
-		
-		for(Usuario u: lista) {
-			System.out.println(u.nome);
-		}
-	}
+    public static void main(String[] args) {
+        List<Usuario> lista = new ArrayList<>();
+
+        Usuario u1 = new Usuario("Ana");
+        lista.add(u1);
+
+        lista.add(new Usuario("Carlos"));
+        lista.add(new Usuario("Lia"));
+        lista.add(new Usuario("Bia"));
+        lista.add(new Usuario("Manu"));
+        lista.add(new Usuario("Manu"));
+
+        System.out.println(lista.get(3)); // acessar pelo índice
+
+        System.out.println(">>>> " + lista.remove(1));
+        System.out.println(lista.remove(new Usuario("Manu")));
+        System.out.println(lista.remove(new Usuario("Manu")));
+        System.out.println(lista.remove(new Usuario("Manu")));
+
+        System.out.println("Tem? " + lista.contains(new Usuario("Lia")));
+        System.out.println("Tem? " + lista.contains(u1));
+
+        for (Usuario u : lista) {
+            System.out.println(u.nome);
+        }
+
+        List<String> teste = new ArrayList<>();
+        teste.add("opa");
+        teste.add("opa2");
+        teste.add("opa3");
+
+        removeLista(teste);
+        verificaLista(teste);
+
+        String testeStr = "opa";
+        System.out.println(testeStr.indexOf("gh"));
+
+    }
+
+    public static void removeLista(List<String> lista) {
+        lista.remove(1);
+    }
+
+    public static void verificaLista(List<String> lista) {
+        System.out.println(lista.get(1));
+    }
 }
--- curso-java/src/lambdas/Fornecedor.java
+++ curso-java/src/lambdas/Fornecedor.java
@@ -6,10 +6,9 @@ import java.util.function.Supplier;
 
 public class Fornecedor {
 
-	public static void main(String[] args) {
-		
-		Supplier<List<String>> umLista = 
-				() -> Arrays.asList("Ana", "Bia", "Lia", "Gui");
-		System.out.println(umLista.get());
-	}
+    public static void main(String[] args) {
+        Supplier<List<String>> umLista =
+                () -> Arrays.asList("Ana", "Bia", "Lia", "Gui");
+        System.out.println(umLista.get());
+    }
 }
